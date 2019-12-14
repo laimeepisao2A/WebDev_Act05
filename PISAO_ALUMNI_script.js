da79a45aa65d1629e61cabb6653fdb0fb83b87fe
laimeepisao2A <58454893+laimeepisao2A@users.noreply.github.com>
2019-12-14 22:48:44 +0800

    Partial

---
+++ PISAO_ALUMNI_script.js
@@ -0,0 +1,8 @@
+function openMenu(){
+	document.getElementById("sideMenu").style.width="450px";
+	document.getElementById("content").style.marginleft="450px";
+}
+function closeMenu(){
+	document.getElementById("sideMenu").style.width="0";
+	document.getElementById("content").style.marginleft="0";
+}
